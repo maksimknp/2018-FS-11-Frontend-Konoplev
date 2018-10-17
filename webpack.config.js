1f1e106b89fcd255d7b21ba733842cc069e93101
Maksim Konoplev <konoplev.md@phystech.edu>
2018-10-17 22:49:47 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: {
-        app: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader', 'eslint-loader'],
     },
-    output: {
-        filename: 'main.js',
-        path: __dirname + '/dist',
-        publicPath: 'dist/'
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [{
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                ]
-            }
-        ]
-    },
-    devtool: 'source-map',
-}
+    {
+      test: /test\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+      }),
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devtool: 'source-map',
+};
